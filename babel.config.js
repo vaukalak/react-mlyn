1e704f11b45c4a24720abfe5668b3581b79dd92d
vaukalak <lahmataja.pa4vara@gmail.com>
2021-11-02 16:33:36 +0300

    added jsx transpilation

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = ["@babel/preset-typescript", "@babel/preset-env"];
-  const plugins = [];
+  const plugins = ["@babel/plugin-syntax-jsx", "@babel/plugin-transform-react-jsx"];
 
   return {
     presets,
