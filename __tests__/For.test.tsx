ff95afbd71d142015337847e8866661954005e34
vaukalak <lahmataja.pa4vara@gmail.com>
2021-12-20 16:59:49 +0300

    added unit tests

---
+++ __tests__/For.test.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { render, act } from "@testing-library/react";
+import { createSubject } from "mlyn";
+import Mlyn, { For } from "../src";
+
+const mapChildren = (container) =>
+  [...container.children].map((c) => c.innerHTML);
+
+test("displays entries", async () => {
+  const items$ = createSubject(["a", "b", "c"]);
+  const { container } = render(
+    <For each={items$} getKey={(e) => e}>
+      {(e$) => <div>{e$()}</div>}
+    </For>
+  );
+  expect(mapChildren(container)).toEqual(["a", "b", "c"]);
+});
+
+test("add / remove entries", async () => {
+  const items$ = createSubject(["a", "b", "c"]);
+  const { container } = render(
+    <For each={items$} getKey={(e) => e}>
+      {(e$) => <div>{e$()}</div>}
+    </For>
+  );
+  expect(mapChildren(container)).toEqual(["a", "b", "c"]);
+  items$([...items$(), "d"]);
+  expect(mapChildren(container)).toEqual(["a", "b", "c", "d"]);
+  items$(items$().slice(1));
+  expect(mapChildren(container)).toEqual(["b", "c", "d"]);
+});
+
+const idGen = () => {
+  let i = 0;
+  return () => (i++).toString();
+};
+
+test("update entries", async () => {
+  const id = idGen();
+  const item = (v) => ({ v, id: id() });
+  const items$ = createSubject([item("a"), item("b"), item("c")]);
+  const { container } = render(
+    <For each={items$} getKey={({ id }) => id}>
+      {(e$) => <Mlyn.div>{e$.v()}</Mlyn.div>}
+    </For>
+  );
+  expect(mapChildren(container)).toEqual(["a", "b", "c"]);
+  act(() => { items$[1].v("d") });
+  expect(mapChildren(container)).toEqual(["a", "b", "c"]);
+});
